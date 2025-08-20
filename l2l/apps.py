a2403412a1819dd0c83e54f1b689e1667d2c36ac
Jonathan Lopez <jonathanglopez@gmail.com>
2025-08-20 09:39:38 -0400

    Format code with ruff

--- l2l/apps.py
+++ l2l/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class L2LConfig(AppConfig):
-    name = 'l2l'
+    name = "l2l"
--- l2l/views.py
+++ l2l/views.py
@@ -6,9 +6,6 @@ from django.template import loader
 
 def index(request):
     now = datetime.now()
-    template = loader.get_template('l2l/index.html')
-    context = {
-        'iso': now.strftime("%Y-%m-%dT%H:%M:%S"),
-        'now': now
-    }
+    template = loader.get_template("l2l/index.html")
+    context = {"iso": now.strftime("%Y-%m-%dT%H:%M:%S"), "now": now}
     return HttpResponse(template.render(context, request))
